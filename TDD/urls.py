3e6bbdb8c546a3ed5c0a04b1eec8f4a68baeeb07
Tymur_Hilfatullin <kevin.shindel@yahoo.com>
2023-03-19 18:11:20 +0200

    refactoring lists app

--- TDD/urls.py
+++ TDD/urls.py
@@ -5,6 +5,6 @@ from lists.views import home_page, shared_page, new_list
 urlpatterns = [
     path('', home_page, name='home'),
     path('lists/shared/', shared_page, name='shared'),
-    path('lists/new/', new_list, name='new_list'),
+    path('lists/new', new_list, name='new_list'),
     path('admin/', admin.site.urls),
 ]
--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render, redirect
-from django.urls import resolve
 
 from lists.models import Item
 
 
 def home_page(request):
-
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST.get('item_text', ''))
-        return redirect('/lists/shared/')
-
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 
 def shared_page(request):
